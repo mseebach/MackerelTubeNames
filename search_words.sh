ef738949e2edc99b5bcfdd48e17d95b329a78cc0
Martin Seebach <mail@martinseebach.dk>
2011-11-04 15:33:56 +0000

    Clean up

--- search_words.sh
+++ search_words.sh
@@ -3,7 +3,7 @@
 # A large number of the words in the file are just possesive versions of another word. 
 # Stripping those to get a cleaner output
 for WORD in `cat /usr/share/dict/words | grep -v "'s" `; do 
-    if [ "`grep -Eiv '['$WORD']' /tmp/tubestations.txt | wc -l`" -eq "1" ]; then 
+    if [ "`grep -Eiv '['$WORD']' tubestations.txt | wc -l`" -eq "1" ]; then 
 	echo $WORD; 
     fi; 
-done > tube-words.txt
+done > mackerel-property-words.txt
